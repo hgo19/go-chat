0ada0b9854563328b5a65c856eadcd65c4b92f9e
Hugo Leonardo <huleonardo19@gmail.com>
2024-11-23 21:28:35 -0300

    chore: remove unecessary code

--- backend/internal/helpers/websocket.go
+++ backend/internal/helpers/websocket.go
@@ -7,11 +7,6 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 func broadcast(msg models.Message) {
 	for conn := range clients {
 		conn.WriteJSON(msg)
